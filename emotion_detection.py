761953b0f8e8165e6ef6f2e0b08dd22cdd85d832
hsaini733 <hsaini@rogers.com>
2026-01-02 00:39:20 -0500

    Added formatting using json and appended dominant_emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,5 @@
 import requests
-
+import json
 def emotion_detector(text_to_analyze):
     """Analyse a given text.
     uses emotion detector
@@ -14,7 +14,11 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json=myobj, headers=header)
-    #formatted_response = json.loads(response.text)
-    
-    return response.text
+    formatted_response = json.loads(response.text)
+    # Assuming your variable is named json_output
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions,key=emotions.get)
+    # 3. Append the dominant_emotion to the dictionary
+    emotions['dominant_emotion'] = dominant_emotion
+    return emotions
     
